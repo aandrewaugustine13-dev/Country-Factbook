dd21ef8f27d04681aeba3d197ba5755675732f81
aandrewaugustine13-dev <aandrewaugustine13@gmail.com>
2026-02-22 23:53:17 -0600

    Delete Country-Factbook directory

--- Country-Factbook/components/StatRow.tsx
+++
@@ -1,8 +0,0 @@
-export function StatRow({ label, value }: { label: string; value: string | number }) {
-  return (
-    <div className="stat-row">
-      <dt>{label}</dt>
-      <dd>{value}</dd>
-    </div>
-  );
-}
