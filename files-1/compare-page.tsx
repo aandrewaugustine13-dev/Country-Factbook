b439df512c527497cc2ec7ff30ae25f52c3e7161
aandrewaugustine13-dev <aandrewaugustine13@gmail.com>
2026-02-24 21:42:19 -0600

    Refactor ComparePage component structure

--- files-1/compare-page.tsx
+++ files-1/compare-page.tsx
@@ -4,15 +4,14 @@ import Link from 'next/link';
 
 export default function ComparePage() {
   return (
-    <div className="container">
-      <Link href="/" className="back-link">
-        ← Back to all countries
-      </Link>
+    <>
+      <Link href="/countries">← Back to all countries</Link>
+
       <h1>Compare Countries</h1>
-      <p style={{ color: '#B7C7DA', marginBottom: '1rem' }}>
-        Select up to 10 countries to compare side by side.
-      </p>
-      <CompareClient countries={comparisonData as any} />
-    </div>
+
+      <p>Select up to 10 countries to compare side by side.</p>
+
+      <CompareClient countries={comparisonData} />   {/* ← this line fixed */}
+    </>
   );
 }
